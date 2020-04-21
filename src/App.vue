c1ce79a7891763458ea395f17272e620d13b2df8
Michael Wales <michaelwales@gmail.com>
2020-04-21 18:40:43 +0100

    Adds all MVP and filtering functionality and minimal styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
-<template>
+<template lang="html">
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+
+    <header>
+      <img src="../src/assets/clipart669455.png" width="175" height="100">
+      <h1>T'wit-T'wooer</h1>
+      <h3>Total T'wit-T'woos => {{totalTweets}}!</h3>
+    </header>
+
+    <div id="filterTweets">
+      <h3>Filter by Likes  <input type="number" v-model.number="filterAmount"></h3>
+    </div>
+
+    <br>
+
+    <twitter-tweet v-for="(tweet, index) in filteredTweets" :key="index" :tweet="tweet"></twitter-tweet>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TwitterTweet from './components/TwitterTweet.vue';
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      tweets:[
+        {
+          id: 1,
+          name: 'James',
+          handle: '@jokerjames',
+          img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+          tweet: "If you don't succeed, dust yourself off and try again.",
+          likes: 10,
+        },
+        {
+          id: 2,
+          name: 'Fatima',
+          handle: '@fantasticfatima',
+          img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+          tweet: "Better late than never but never late is better.",
+          likes: 12,
+        },
+        {
+          id: 3,
+          name: 'Xin',
+          handle: '@xeroxin',
+          img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+          tweet: "Beauty in the struggle, ugliness in the success.",
+          likes: 18,
+        }
+      ],
+      filterAmount: 0
+    }
+  },
   components: {
-    HelloWorld
+    'twitter-tweet': TwitterTweet
+  },
+  computed: {
+    totalTweets: function(){
+      return this.tweets.reduce((total, tweet) => total + tweet.likes, 0)
+    },
+    filteredTweets: function(){
+      return this.tweets.filter((tweet) => {
+        return tweet.likes >= this.filterAmount
+      })
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="css" scoped>
+
+header {
+  font-family: 'Viga';
 }
+
+#filterTweets {
+  font-family: 'Viga';
+}
+
+#app {
+  font-family: 'Viga';
+}
+
 </style>
---
+++ src/components/TwitterTweet.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    <img v-bind:src="tweet.img" width="40" height="40" v-bind:title="tweet.name">
+    <p>{{tweet.name}} {{tweet.handle}}</p>
+    <p>"{{tweet.tweet}}"</p>
+    <p>Likes: {{tweet.likes}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'twitter-tweet',
+  props: ['tweet']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
